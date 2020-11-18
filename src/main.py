21c2569398303d2464d59039804db5aa53d61901
maoyuching <18270414620@139.com>
2020-11-18 16:32:07 +0800

    开启新的线程维护一个护眼屏

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+import threading
 from tkinter import *
 
 
@@ -34,31 +35,35 @@ class EyeRelax:
         self.work_time = int(self.ety_wkt.get() if self.ety_wkt.get() != '' else 10)
         self.relax_time = int(self.ety_rlt.get() if self.ety_rlt.get() != '' else 10)
 
-        new_window = Toplevel(self.root, bg="green")
-        new_window.overrideredirect(True)
-        new_window.geometry("1920x1080")
+        while True:
+            new_window = Toplevel(self.root, bg="green")  # toplevel 是一个弹出框组件
+            new_window.overrideredirect(True)  # 无边框
+            new_window.geometry("1920x1080")
 
-        # 设置 护眼 屏保 文字
-        lb_hint = Label(new_window, text="relaxing.....", bg="green")
-        lb_hint.pack()
+            # 设置 护眼 屏保 文字
+            lb_hint = Label(new_window, text="relaxing.....", bg="green")
+            lb_hint.pack()
 
-        self.root.focus_force()
-        new_window.focus_force()  # 用来给组件获取焦点
+            self.root.focus_force()  # 强制获得焦点
+            new_window.bind("<FocusOut>", new_window.focus_force())
+            new_window.update()  # update方法和 withdraw 相反，将隐藏的窗口展现出来
+            for j in range(self.relax_time, 0, -1):  # 开始休息指定的秒数
+                new_window.focus_force()  # 用来给组件获取焦点
+                lb_hint["text"] = j
+                self.root.update()
+                time.sleep(1)
 
-        new_window.update()  # update方法和 withdraw 相反，将隐藏的窗口展现出来
-        new_window.bind("<FocusOut>", new_window.focus_force())
-        # 开始休息指定的秒数
-        for j in range(self.relax_time, 0, -1):
-            lb_hint["text"] = j
-            self.root.update()
-            time.sleep(1)
+            # 隐藏屏保，蛰伏指定的秒数，然后重又出现
+            new_window.withdraw()  # withdraw 意思是隐藏
+            print(self.log_line + "蛰伏")
+            time.sleep(self.work_time)
+            print(self.log_line + "回复")
 
-        # 隐藏屏保，蛰伏指定的秒数，然后重又出现
-        new_window.withdraw()  # withdraw 意思是隐藏
-        print(self.log_line + "蛰伏")
-        time.sleep(self.work_time)
-        print(self.log_line + "起来")
-        self.show()
+    def show_threading(self):
+        print(self.log_line + "启动线程")
+        t = threading.Thread(target=self.show, name="thread_show")
+        t.setDaemon(True)
+        t.start()
 
     def start(self):
         """启动程序"""
@@ -73,7 +78,7 @@ class EyeRelax:
         self.lb_rlt.pack()
         self.ety_rlt.pack()
 
-        self.btn_wkt.configure(text="OK", command=self.show)
+        self.btn_wkt.configure(text="OK", command=self.show_threading)
         self.btn_wkt.pack()
         self.btn_rlt.configure(text="quit", command=self.quit)
         self.btn_rlt.pack()
